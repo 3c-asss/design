e193f6280979857ef0cc8f70d4884f35895fb58b
1123354698@qq.com <1123354698@qq.com>
2022-05-04 16:49:54 +0800

    jiayou

--- app/src/main/java/com/example/design/researchActivity2.java
+++ app/src/main/java/com/example/design/researchActivity2.java
@@ -2,17 +2,16 @@ package com.example.design;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class researchActivity2 extends AppCompatActivity implements View.OnClickListener {
 private Button btn_mima2,btn_reg1,yanzhengma;
-private EditText phone_number;
+private EditText PN;
+String telphone;
     private Object researchActivity2;
 
     @Override
@@ -23,8 +22,9 @@ private EditText phone_number;
         initView();
         //验证手机号是否正确
         //测试
-        EditText phone_number=(EditText)findViewById(R.id.phone_number);
-        String telphone=phone_number.getText().toString();
+      //获取可编辑文本框内容
+        EditText PN=(EditText)findViewById(R.id.phone_number);
+
         btn_mima2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,26 +50,11 @@ private EditText phone_number;
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.yanzhengma:
-                String telphone=phone_number.getText().toString();
-                boolean judge = isMobileNO(telphone);
-                if (judge == true) {
-                    Intent intent_insert = new Intent();
-                    intent_insert.setClass(researchActivity2.this, nav_MainActivity2.class);
-                    startActivity(intent_insert);
-                } else {
-                    Toast.makeText(this, "手机号码错误", Toast.LENGTH_SHORT).show();
-                }
-                break;
+               // String toString=phone_number.getText().toString();
+
         }
     }
 
     //验证手机号
-    public  static boolean isMobileNO (String telphone) {
-        String telRegex = "[1][358]\\d{9}";//"[1]"代表第1位为数字1，"[358]"代表第二位可以为3、5、8中的一个，"\\d{9}"代表后面是可以是0～9的数字，有9位。
-        if (TextUtils.isEmpty(telphone)){
-            return false;
-        }
-        else
-            return telphone.matches(telRegex);
-    }
+
 }
